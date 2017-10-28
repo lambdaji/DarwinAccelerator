8ed45c3d7ec2f1f5e104f8c7b3fe576da9474790
lambdaji <jzygle@gmail.com>
2017-10-28 20:39:00 +0800

    update
    update class

---
+++ src/DarwinAccelerator.cpp
@@ -0,0 +1,11 @@
+/*
+ * DarwinAccelerator.cpp
+ *
+ *  Created on: 2017Äê10ÔÂ28ÈÕ
+ *      Author: lambdaji
+ */
+
+#include "DarwinAccelerator.h"
+#include
+
+
---
+++ src/DarwinAccelerator.h
@@ -0,0 +1,20 @@
+/*
+ * DarwinAccelerator.h
+ *
+ *  Created on: 2017Äê10ÔÂ28ÈÕ
+ *      Author: lambdaji
+ */
+
+#ifndef DARWINACCELERATOR_H_
+#define DARWINACCELERATOR_H_
+
+#include "expinfo.pb.h"
+
+class DarwinAccelerator
+{
+
+};
+
+
+
+#endif /* DARWINACCELERATOR_H_ */
